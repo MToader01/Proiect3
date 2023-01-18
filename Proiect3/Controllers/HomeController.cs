0f663f6316df2abbb9d7b086851988f7153b7edf
Pastina Marcel Toader <pastina.marcel.toader@gmail.com>
2023-01-18 20:56:45 +0200

    Initial commit

---
+++ Proiect3/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Proiect3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Diagnostics;
+
+namespace Proiect3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login() 
+        {
+            Trace.WriteLine("Login Page opened");
+            return RedirectToAction("Login", "Account");
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Proiect3/Models/BuyedProductModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Proiect3.Models
+{
+    public class BuyedProductModel
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class BuyedProductModelDbContext : DbContext
+    {
+        public DbSet<BuyedProductModel> BuyedProducts { get; set;}
+    }
+}
